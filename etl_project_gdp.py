4a4561e8c1b134320f5a8fff54f3effee0e12ee5
BonganiChauke <bonganichauke11@outlook.com>
2025-04-30 13:50:52 +0200

    Function to transform data column from currency to float

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -36,3 +36,12 @@ def extract(url, table_attribs):
                 df1 = pd.DataFrame(data_dict, index=[0])
                 df = pd.concat([df,df1], ignore_index=True)
     return df
+
+# this function to convert GDP information from currency to float value
+def transform(df):
+    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000,2) for x in GDP_list]
+    df["GDP_USD_millions"] = GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    return df
