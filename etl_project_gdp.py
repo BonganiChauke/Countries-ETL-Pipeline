5f3dc98a16e9a106db02dc3ce8bc39f9c88e5ab0
BonganiChauke <bonganichauke11@outlook.com>
2025-04-30 13:45:01 +0200

    Function for Extracting information from a web page append it into a pandas data frame

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -12,3 +12,27 @@ table_attribs = ['Country', 'GDP_USD_millions']
 db_name = 'World_Economies.db'
 table_name = 'Countries_by_GDP'
 csv_path = './Countries_by_GDP.csv'
+
+# this function extracts the required information from the website and saves into a pandas dataframe. 
+# The function returns a dataframe
+def extract(url, table_attribs):
+    # parsing html page into Beautiful Soup as text
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    
+    # extracting html table information
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    
+    # to retrieve data all at once
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                # appending data into dataframe
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
